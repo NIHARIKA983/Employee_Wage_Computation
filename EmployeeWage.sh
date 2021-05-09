822795cebea313974415bd5d944c6787da6f876a
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-05-09 11:39:23 +0530

    Added:Calculating Waages for a Month

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -61,3 +61,20 @@ case $empCheck in
 esac
 salaray=$(($empHrs*$empRatePerHr));
 
+totalSalary=0;
+numWorkingDays=20;
+
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+   empCheck=$((RANDOM%3));
+          case $empCheck in
+                 $isFullTime)
+                     empHrs=8;;
+               $isPartTime)
+                     empHrs=4;;
+               *)
+                     empHrs=0;;
+         esac
+   salary=$(($empHrs*$empRatePerHr));
+   totalSalary=$(($totalSalary+$salary));
+done
